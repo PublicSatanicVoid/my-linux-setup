cd9ad55c0ece598a388e898a16cd6d3e030d2c01
Adam Priebe <adam.priebe@broadcom.com>
2024-02-28 12:01:20 -0600

    :WhereCopy command to show current file path and copy to clipboard via OSC52

--- config/nvim/lua/func.lua
+++ config/nvim/lua/func.lua
@@ -1,9 +1,19 @@
 -- Function to execute 'readlink -f' on the current file
 local function where()
-  local filePath = vim.api.nvim_buf_get_name(0)
-  local result = vim.fn.system("readlink -f " .. filePath)
-  print(result)
+    local filePath = vim.api.nvim_buf_get_name(0)
+    local result = vim.fn.system("readlink -f " .. filePath)
+    print(result)
+end
+
+local function whereCopy()
+    local filePath = vim.api.nvim_buf_get_name(0)
+    local result = vim.fn.system("readlink -f " .. filePath)
+    print(result)
+
+    result = result:gsub("\n", "")
+    vim.fn.OSCYank(result)
 end
 
 -- Register the ':Where' command
 vim.api.nvim_create_user_command('Where', where, {})
+vim.api.nvim_create_user_command('WhereCopy', whereCopy, {})
