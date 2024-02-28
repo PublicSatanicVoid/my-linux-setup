d137c27bf9fc0e772ac8f412e7e6d21795ec0e49
Adam Priebe <adam.priebe@broadcom.com>
2024-02-28 12:08:21 -0600

    improve highlighting of quick-scope

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -1,3 +1,11 @@
+vim.cmd [[
+augroup qs_colors
+  autocmd!
+  autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+  autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+augroup END
+]]
+
 require("plugins")
 require("set")
 require("remap")
