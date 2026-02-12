9f83421a6c9ee64ee0bea458f2a0994e6d34a78e
Adam Priebe <adam.priebe@broadcom.com>
2026-02-12 12:18:46 -0600

    nvim: hide duplicate blink.cmp completions

--- config/nvim/lua/plugins_specs.lua
+++ config/nvim/lua/plugins_specs.lua
@@ -130,6 +130,7 @@ local T = {
             },
             sources = {
                 default = { "lsp", "buffer" },
+                priority = { "lsp", "buffer" },
 
                 providers = {
                     -- https://cmp.saghen.dev/configuration/sources#show-buffer-completions-with-lsp
@@ -154,7 +155,28 @@ local T = {
                     show_documentation = true
                 }
             }
-        }
+        },
+
+        -- https://github.com/saghen/blink.cmp/issues/1222#issuecomment-2891921393
+        config = function(_, opts)
+            local original = require("blink.cmp.completion.list").show
+            ---@diagnostic disable-next-line: duplicate-set-field
+            require("blink.cmp.completion.list").show = function(ctx, items_by_source)
+                local seen = {}
+                local function filter(item)
+                    if seen[item.label] then return false end
+                    seen[item.label] = true
+                    return true
+                end
+
+                for id in vim.iter(opts.sources.priority) do
+                    items_by_source[id] = items_by_source[id] and vim.iter(items_by_source[id]):filter(filter):totable()
+                end
+                return original(ctx, items_by_source)
+            end
+
+            require("blink.cmp").setup(opts)
+        end
     },
 
     {
